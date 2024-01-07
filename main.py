e971ae8b3130de237b176280aa92b411ef88640f
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-01-07 01:14:59 -0300

    0.0.1

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:# Se verifica si el usuario cerro la ventana
             running = False
-    
-    screen.fill((255, 255, 255))# Se pinta el fondo de la ventana
+    screen.fill((0, 0, 0))# Se pinta el fondo de la ventana
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)# Se dibuja un círculo azul en el centro
     pygame.display.flip()# Muestra los cambios en la pantalla
 pygame.quit() # Fin
