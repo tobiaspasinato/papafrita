b0074d5fdfb858dc0c02175c7544d77be6f5f0ed
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-01-06 21:29:17 -0300

    0.0.1 tob

---
+++ functions.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+import math
+
+def get_animacion(path : str, columnas : int, filas : int, flip = False):
+    lista = []
+    surface_imagen = pygame.image.load(path)
+    fotograma_ancho = int(surface_imagen.get_width()/columnas)
+    fotograma_alto = int(surface_imagen.get_height()/filas)
+    x = 0
+    for columna in range(columnas):
+        for fila in range(filas):
+            x = columna * fotograma_ancho
+            y = fila * fotograma_alto
+            surface_fotograma = surface_imagen.subsurface(x, y, fotograma_ancho, fotograma_alto)
+            if flip == True:
+                surface_fotograma = pygame.transform.flip(surface_fotograma, True, False)
+            lista.append(surface_fotograma)
+    return lista
